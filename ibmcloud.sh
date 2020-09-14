1bf90d707b414b506967cdfacb9bad4b8aba2864
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-14 20:59:12 +0800

    Update ibmcloud.sh

--- ibmcloud.sh
+++ ibmcloud.sh
@@ -4,31 +4,19 @@ read -p "请设置你的容器内存大小(默认256):" ramsize
 if [ -z "$ramsize" ];then
 	ramsize=256
 fi
-rm -rf cloudfoundry
-mkdir cloudfoundry
-cd cloudfoundry
-
-echo '<!DOCTYPE html> '>>index.php
-echo '<html> '>>index.php
-echo '<body>'>>index.php
-echo '<?php '>>index.php
-echo 'echo "Hello World!"; '>>index.php
-echo '?> '>>index.php
-echo '<body>'>>index.php
-echo '</html>'>>index.php
-
+cd get-started-python
 wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip -d v2ray1 v2ray-linux-64.zip
 cd v2ray1
 chmod 777 *
 cd ..
 rm -rf v2ray-linux-64.zip
-mv $HOME/cloudfoundry/v2ray1/v2ray $HOME/cloudfoundry/v2ray
-mv $HOME/cloudfoundry/v2ray1/v2ctl $HOME/cloudfoundry/v2ctl
-rm -rf $HOME/cloudfoundry/v2ray1
+mv ~/get-started-python/v2ray1/v2ray ~/get-started-python/v2ray
+mv ~/get-started-python/v2ray1/v2ctl ~/get-started-python/v2ctl
+rm -rf ~/get-started-python/v2ray1
 uuid=`cat /proc/sys/kernel/random/uuid`
 path=`echo $uuid | cut -f1 -d'-'`
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>~/get-started-python/config.json
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
 echo '  command: '/app/htdocs/v2ray'' >>manifest.yml
