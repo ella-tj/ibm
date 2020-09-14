fb6c25aa36c92e78724f0ac669e6a58f6f036305
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-14 21:32:10 +0800

    Update ibmcloud.sh

--- ibmcloud.sh
+++ ibmcloud.sh
@@ -17,14 +17,6 @@ rm -rf ~/get-started-python/v2ray1
 uuid=`cat /proc/sys/kernel/random/uuid`
 path=`echo $uuid | cut -f1 -d'-'`
 echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>~/get-started-python/config.json
-cd ~/get-started-python
-ibmcloud target --cf
-ibmcloud cf push
-domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
-vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
-cd ..
-echo 容器已经成功启动
-echo 地址: $domain
 echo UUID: $uuid
 echo path: /$path
 echo vmess://$vmess
