3ff81312de33443610e738c66a1687a94cbc63ae
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-16 07:22:31 +0800

    Update ibmcloud.sh

--- ibmcloud.sh
+++ ibmcloud.sh
@@ -2,7 +2,7 @@
 #read -p "请设置你的容器内存大小(默认256):" ramsize
 #if [ -z "$ramsize" ];then
 #	ramsize=256
-#fi
+fi
 cd $HOME/get-started-python
 wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip -d v2ray1 v2ray-linux-64.zip
@@ -13,9 +13,9 @@ rm -rf v2ray-linux-64.zip
 mv $HOME/get-started-python/v2ray1/v2ray $HOME/get-started-python/vray
 mv $HOME/get-started-python/v2ray1/v2ctl $HOME/get-started-python/v2ctl
 rm -rf $HOME/get-started-python/v2ray1
-#uuid=`cat /proc/sys/kernel/random/uuid`
-#path=`echo $uuid | cut -f1 -d'-'`
-#echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/get-started-python/cc/config.json
+uuid=`cat /proc/sys/kernel/random/uuid`
+path=`echo $uuid | cut -f1 -d'-'`
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/get-started-python/config.json
 #ibmcloud target --cf
 #ibmcloud cf push
 #domain=`ibmcloud cf app GetStartedPython | grep routes | cut -f2 -d':' | sed 's/ //g'`
